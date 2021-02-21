3ec9cf2e2b282dab09fc6d04181c8e83b58bf2da
Shane R <imspammails@gmail.com>
2021-02-21 23:29:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
   collection.each do |key, value|
-    
+    binding.pry
   end
 end
 
