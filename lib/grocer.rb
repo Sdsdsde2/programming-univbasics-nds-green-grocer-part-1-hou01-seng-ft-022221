fb3ae5df8e0d22232599f1d68035f0adb2199d90
Shane R <imspammails@gmail.com>
2021-02-21 23:39:28 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,12 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  new_hash = { }
+  cart.each do |key|
+    key.each do |inner_key, inner_value|
+      binding.pry
+    end
+  end
 end
 
 
